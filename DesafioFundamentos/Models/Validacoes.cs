cdcb76d387b990be5c8b1c5cc14d002642eee00f
Kaique Martins <kaiquems.e@gmail.com>
2023-09-28 22:46:08 -0300

    Correção na implementação dos métodos e criação da classe Validacoes

---
+++ DesafioFundamentos/Models/Validacoes.cs
@@ -0,0 +1,24 @@
+﻿namespace DesafioFundamentos.Models
+{
+    public class Validacoes
+    {
+        public static bool ValidarPlaca(string placa)
+        {
+            bool placaValida = true;
+            int totalDigitos = 7;
+
+            if (string.IsNullOrWhiteSpace(placa))
+            {
+                Console.WriteLine("Nenhum valor informado! Tente novamente");
+                placaValida = false;
+            }
+            else if (placa.Length > totalDigitos)
+            {
+                Console.WriteLine($"Placa inválida! Confira se digitou a placa corretamente, uma placa válida contem {totalDigitos} digitos");
+                placaValida = false;
+            }
+
+            return placaValida;
+        }
+    }
+}
